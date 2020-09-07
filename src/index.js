4da469a4eb61b98c84d19c5e836cdf1170effbd4
Shashank Biplav <Biplavshashank7@gmail.com>
2020-09-08 04:38:40 +0530

    Basic Reducer & Store setup

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
 
-const app= (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+const store = createStore(reducer);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
 ReactDOM.render(
-  <React.StrictMode>
-      {app}
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        {app}
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initalState = {
+    ingredients: null,
+    totalPrice: 30
+};
+
+const reducer = (state = initalState, action) => {
+
+};
+
+export default reducer;
